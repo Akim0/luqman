717b9251cbf2b4735686c05becc91ebc0c491ad5
ICTKVPJB <ICTKVPJB@ICTKVPJB-PC>
2018-01-18 10:19:57 +0800

    syaqir

---
+++ NetBeansProjects/ProjectGitHub/src/project1.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+class kpd
+{
+    public static void main(String args[])
+    {
+       Scanner input=new Scanner(System.in);
+       System.out.print("Masukkan Bil : ");
+       int bil=input.nextInt();
+       
+       System.out.print("Masukkan Kelas : ");
+       String kelas=input.next();
+       
+       System.out.print("Masukkan Guru Kelas : ");
+       String guru=input.next();
+       
+        System.out.print("Masukkan Bil : ");
+       int bil2=input.nextInt();
+       
+       System.out.print("Masukkan Kelas : ");
+       String kelas2=input.next();
+       
+       System.out.print("Masukkan Guru Kelas : ");
+       String guru2=input.next();
+       
+        System.out.print("Masukkan Bil : ");
+       int bil3=input.nextInt();
+       System.out.print("Masukkan Kelas : ");
+       String kelas3=input.next();
+       
+       System.out.print("Masukkan Guru Kelas : ");
+       String guru3=input.next();
+       
+        System.out.print("Masukkan Bil : ");
+       int bil4=input.nextInt();
+       
+       System.out.print("Masukkan Kelas : ");
+       String kelas4=input.next();
+       
+       System.out.print("Masukkan Guru Kelas : ");
+       String guru4=input.next();
+       
+        System.out.print("Masukkan Bil : ");
+       int bil5=input.nextInt();
+       
+       System.out.print("Masukkan Kelas : ");
+       String kelas5=input.next();
+       
+       System.out.print("Masukkan Guru Kelas : ");
+       String guru5=input.next();
+       
+        System.out.print("Masukkan Bil : ");
+       int bil6=input.nextInt();
+       
+       System.out.print("Masukkan Kelas : ");
+       String kelas6=input.next();
+       
+       System.out.print("Masukkan Guru Kelas : ");
+       String guru6=input.next();
+       
+        System.out.print("Masukkan Bil : ");
+       int bil7=input.nextInt();
+       
+       System.out.print("Masukkan Kelas : ");
+       String kelas7=input.next();
+       System.out.print("Masukkan Guru Kelas : ");
+       String guru7=input.next();
+       
+       System.out.println("Bil \t\t Kelas \t\t Guru Kelas");
+       System.out.println(bil+"\t\t "+kelas+"\t\t "+guru);
+       System.out.println("Bil \t\t Kelas \t\t Guru Kelas");
+       System.out.println(bil2+"\t\t "+kelas2+"\t\t "+guru2);
+       System.out.println("Bil \t\t Kelas \t\t Guru Kelas");
+       System.out.println(bil3+"\t\t "+kelas3+"\t\t "+guru3);
+       System.out.println("Bil \t\t Kelas \t\t Guru Kelas");
+       System.out.println(bil4+"\t\t "+kelas4+"\t\t "+guru4);
+       System.out.println("Bil \t\t Kelas \t\t Guru Kelas");
+       System.out.println(bil5+"\t\t "+kelas5+"\t\t "+guru5);
+       System.out.println("Bil \t\t Kelas \t\t Guru Kelas");
+       System.out.println(bil6+"\t\t "+kelas6+"\t\t "+guru6);
+       System.out.println("Bil \t\t Kelas \t\t Guru Kelas");
+       System.out.println(bil7+"\t\t "+kelas7+"\t\t "+guru7);
+       
+      
+    }
+        
+            
+            
+}
+
